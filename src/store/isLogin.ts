9bb5e6f5206492cbbfa1d8c987e1aa2f2bac5715
xiangGao <hello@baoziv587.com>
2017-07-28 10:32:47 +0800

    rename

--- src/store/isLogin.ts
+++ src/store/isLogin.ts
@@ -1,5 +1,6 @@
 import store from './configure-store'
 import toJs from '../utils/immutable-to-js'
+
 export default () => {
   const jsStore = toJs(store.getState()) as any
   const session = jsStore.session
