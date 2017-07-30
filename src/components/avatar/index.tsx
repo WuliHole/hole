e05ef10d3d61f9acb229ebd0f8d2a7b9b3aa0c57
xiangGao <hello@baoziv587.com>
2017-07-30 08:41:32 +0800

    new logo

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -1,13 +1,17 @@
 import React = require('react')
 import Container from '../container'
 import './style.less'
+const defaultAvatar = require('../../assets/excited.svg')
 interface Props {
   src: string
   size?: number
   style?: React.CSSProperties
   onClick?: any
 }
-export default ({ src = '', size = 64, style = {}, onClick }: Props) => {
+export default ({ src = defaultAvatar, size = 64, style = {}, onClick }: Props) => {
+  if (!src) {
+    return
+  }
 
   return <img style={ {
     width: `${size}px`,
