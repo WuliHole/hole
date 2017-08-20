6ac557f401b995086584d4ea8ca37a85feb35dec
xiangGao <hello@baoziv587.com>
2017-08-20 18:50:13 +0800

    AutoSave plugin

---
+++ src/components/editor/plugins/autoSave/autosave.plugin.ts
@@ -0,0 +1,62 @@
+import { EditorPluginBuilder } from '../interface.plugin'
+import { EditorState } from 'draft-js'
+import { is } from 'immutable'
+import debounce from '../../../../utils/debounce'
+
+interface PluginConfig {
+  /**
+   * save function, async or sync
+   */
+  saveAction: (editorState: EditorState) => any
+  /**
+   * define how long the saveAction  be invoked
+   * @default 500
+   */
+  debounceTime?: number
+}
+
+interface Method {
+  save: () => void
+}
+export const createAutoSavePlugin: EditorPluginBuilder<PluginConfig, Method> = ({ debounceTime = 500, saveAction }) => {
+
+
+  if (typeof saveAction !== 'function') {
+    throw new TypeError(`[createAutoSavePlugin]: saveAction is not function`)
+  }
+
+  let prevState: EditorState
+  let currentState: EditorState
+  saveAction = debounce(saveAction, debounceTime)
+
+  function differentState() {
+    return currentState && !is(prevState, currentState)
+  }
+
+  function save() {
+    if (differentState()) {
+      saveAction(currentState)
+    }
+  }
+
+  return {
+    initialize: ({ setEditorState, getEditorState }) => {
+      const state = getEditorState()
+      prevState = state
+      currentState = state
+    },
+    willUnMount() {
+      prevState = null
+      currentState = null
+    },
+    onChange: (editorState) => {
+      prevState = currentState
+      currentState = editorState
+      save()
+      return editorState
+    },
+    save,
+  }
+}
+
+export default createAutoSavePlugin
---
+++ src/components/editor/plugins/interface.plugin.ts
@@ -0,0 +1,41 @@
+import React = require('react')
+import { EditorState } from 'draft-js'
+/**
+ * This file defined the interface of plugin.
+ * Each plugin you created should implement EditorPluginBuilder
+ * All details of draft-js-plugin,see https://github.com/draft-js-plugins/draft-js-plugins/blob/master/HOW_TO_CREATE_A_PLUGIN.md
+ */
+
+export type EditorPluginBuilder<T, R> = (pluginConfig: T) => InAddtionAccepts & R
+
+
+
+interface PluginFunctions {
+  // a function returning a list of all the plugins
+  getPlugins,
+  // a function returning a list of all the props pass into the Editor
+  getProps,
+  // a function to update the EditorState
+  setEditorState: (editorState: EditorState) => void,
+  // a function to get the current EditorState
+  getEditorState: () => EditorState,
+  // a function returning of the Editor is set to readOnly
+  getReadOnly,
+  // a function which allows to set the Editor to readOnly
+  setReadOnly,
+  // a function to get the editor reference
+  getEditorRef,
+}
+
+type Decorator = {
+  strategy: any,
+  component: React.ComponentClass<any>,
+}
+
+interface InAddtionAccepts {
+  initialize?: (props: PluginFunctions) => void
+  onChange?: (editorState: EditorState) => EditorState
+  willUnMount?: (props: PluginFunctions) => void
+  decorators?: (decorators: Decorator[]) => void
+  getAccessibilityProps?: () => { ariaHasPopup: string, ariaExpanded: string }
+}
