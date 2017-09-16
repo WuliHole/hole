08c684f55e18a049c520b9fa3a81938efad7ca79
xiangGao <hello@baoziv587.com>
2017-09-16 11:29:58 +0800

    [Engine]:qn-webpack plugin for prodction

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -6,7 +6,11 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManiFest = require('webpack-manifest-plugin');
 const QiniuPlugin = require('qn-webpack');
-const config = require('../qiniu.json')
+let config = {}
+
+if (process.env.NODE_ENV === 'production') {
+  config = require('../qiniu.json')
+}
 
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
 //   .BundleAnalyzerPlugin;
@@ -75,13 +79,13 @@ const prodPlugins = [
     ),
   }),
 
-  // new QiniuPlugin({
-  //     accessKey: config.accessKey,
-  //     secretKey: config.secretKey,
-  //     bucket: config.bucket,
-  //     path: config.path,
-  //     zone: config.zone,
-  // }),
+  new QiniuPlugin({
+    accessKey: config.accessKey,
+    secretKey: config.secretKey,
+    bucket: config.bucket,
+    path: config.path,
+    zone: config.zone,
+  }),
 
   new webpack.optimize.DedupePlugin(),
 
