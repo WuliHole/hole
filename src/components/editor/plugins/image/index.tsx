80317dae6bed93307ef0fe21257ad2b95fd4a5cb
xiangGao <hello@baoziv587.com>
2017-09-30 15:35:04 +0800

    Style improvement for image alignment

--- src/components/editor/plugins/image/index.tsx
+++ src/components/editor/plugins/image/index.tsx
@@ -1,8 +1,33 @@
 import createPlugin from 'draft-js-image-plugin'
-import { EditorState } from 'draft-js'
-
+import { EditorState, ContentBlock } from 'draft-js'
+import './style.less'
 export const createImagePlugin = (config?: PluginConfig): ImagePluginObject => {
-  return createPlugin(config)
+  return {
+    ...createPlugin(config),
+    blockStyleFn(block: ContentBlock, { getEditorState }) {
+      const entityKey = block.getEntityAt(0);
+      const contentState = getEditorState().getCurrentContent();
+      const alignmentData = entityKey ? contentState.getEntity(entityKey).data : {};
+      /* tslint:disable */
+      console.log('image', alignmentData)
+      /* tslint:enable */
+      if (alignmentData.src) {
+        switch (alignmentData.alignment) {
+          case 'left':
+            return 'img-align-left'
+
+          case 'right':
+            return 'img-align-right'
+
+          case 'center':
+            return 'img-align-center'
+
+          case 'default':
+            return
+        }
+      }
+    }
+  }
 }
 
 interface PluginConfig {
