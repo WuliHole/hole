7608c6fbd2297c21af8bf7ad8a68243b10fe9110
xiangGao <hello@baoziv587.com>
2017-09-30 15:33:17 +0800

    clean code

--- src/components/editor/plugins/code-highlight/code-light.plugin.tsx
+++ src/components/editor/plugins/code-highlight/code-light.plugin.tsx
@@ -1,27 +1,18 @@
 import React = require('react')
 import { EditorPluginBuilder, InAddtionAccepts } from '../interface.plugin'
 import { EditorState, ContentBlock, ContentState } from 'draft-js'
-import decorateComponentWithProps from 'decorate-component-with-props';
 import Prism = require('prismjs')
 import PrismDecorator = require('draft-js-prism')
 import './style.less'
 import './laguage'
 
-interface PluginConfig {
-  syntax?: string
-}
-
+interface PluginConfig { }
 interface Method { }
-
-
 export type codeHighLightPlugin = InAddtionAccepts & Method
 
-const P = ({ type, children, syntax }) => <span className={ `prism-token token ${type} ` }>{ children }</span>
 const WS = /\s+/g
 const BACK_SLASH = /\//g
-
 const mapFileExtensionToLanguage = (name: string) => FILE_EXTENSION_MAP[name] ? FILE_EXTENSION_MAP[name] : name
-
 export const FILE_EXTENSION_MAP = {
   js: 'javascript',
   'c++': 'cpp',
@@ -30,50 +21,45 @@ export const FILE_EXTENSION_MAP = {
   rb: 'ruby'
 }
 
-export const codeHighLightPlugin: EditorPluginBuilder<PluginConfig, Method> = ({ syntax = 'css' }) => {
+export const codeHighLightPlugin: EditorPluginBuilder<PluginConfig, Method> = (...args) => {
 
   let currentSyntax
+
+  // "//${type}"           it'sworkring
+  // "//   ${type}   "     it'sworkring
+  // "/*${type}*/"         not working
+  // support file extension name. ie: js=>javascript , ts=>typescript
+  function getSyntax(block: ContentBlock) {
+    const text = block.getText().replace(WS, '')
+
+    if (!text || !text.startsWith('//')) {
+      return currentSyntax
+    }
+
+    let languageType = text.replace(BACK_SLASH, '')
+    languageType = mapFileExtensionToLanguage(languageType)
+    const syntaxDefinition = Prism.languages[languageType]
+
+    if (typeof syntaxDefinition === 'object') {
+      currentSyntax = languageType
+    }
+
+    if (__DEV__) {
+      /* tslint:disable */
+      console.log(text, languageType, syntaxDefinition, currentSyntax)
+      /* tslint:enable */
+    }
+    return currentSyntax
+  }
+
+  function render({ type, children }) {
+    return (
+      <span className={ `prism-token token ${type}` }>{ children }</span>
+    )
+  }
+
   return {
-    initialize() {
-
-    },
-    decorators: [
-      new PrismDecorator({
-        prism: Prism,
-
-        // "//${type}"           it'sworkring
-        // "//   ${type}   "     it'sworkring
-        // "/*${type}*/"         not working
-        // support file extension name. ie: js=>javascript , ts=>typescript
-        getSyntax(block: ContentBlock) {
-          const text = block.getText().replace(WS, '')
-
-          if (!text || !text.startsWith('//')) {
-            return currentSyntax
-          }
-
-          let languageType = text.replace(BACK_SLASH, '')
-          languageType = mapFileExtensionToLanguage(languageType)
-          const syntaxDefinition = Prism.languages[languageType]
-
-          if (typeof syntaxDefinition === 'object') {
-            currentSyntax = languageType
-          }
-
-          if (__DEV__) {
-            /* tslint:disable */
-            console.log(text, languageType, syntaxDefinition, currentSyntax)
-            /* tslint:enable */
-          }
-          return currentSyntax
-        },
-        render({ type, children }) {
-          return (
-            <span className={ `prism-token token ${type}` }>{ children }</span>
-          );
-        },
-      }),
-    ],
+    decorators: [new PrismDecorator({ prism: Prism, getSyntax, render })],
   }
 }
 
