e9c5e2dbc3cfc7b3e1697d55f0e6336c3fa30ecb
xiangGao <hello@baoziv587.com>
2017-09-30 18:16:27 +0800

    Enhance function resolveTitleFromContent

--- src/utils/resolveTitleFromContent.ts
+++ src/utils/resolveTitleFromContent.ts
@@ -2,13 +2,27 @@ import { RawDraftContentState } from 'draft-js';
 const defaultTitle = '未命名'
 const SPACE = /\s+/
 const MAX_LENGTH = 20
+const BACK_SLASH = /\//g
+const filterBlocks = ['atomic', 'code-block']
+const filterKeyWord = [SPACE, BACK_SLASH]
+
 function resolveTitleFromContent(content: RawDraftContentState): string {
   if (!content) {
     return
   }
-  const firstBlock = content.blocks[0]
-  if (firstBlock) {
-    const text = firstBlock.text && firstBlock.text.replace(SPACE, '')
+
+  let targetBlock
+  const len = content.blocks.length
+  for (let i = 0; i < len; i++) {
+    const block = content.blocks[i]
+    if (filterBlocks.indexOf(block.type) === -1) {
+      targetBlock = block
+      break
+    }
+  }
+
+  if (targetBlock) {
+    const text = targetBlock.text && filterKeyWord.reduce((acc, rule) => acc.replace(rule, ''), targetBlock.text)
 
     if (!text) {
       return defaultTitle
